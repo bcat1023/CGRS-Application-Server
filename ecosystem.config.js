84b3970ae63e2ae10e821a026ee2cad3083145e3
Brandon Gillen <brandon@nookalley.com>
2026-01-27 13:34:48 -0800

    Adding deployment config (test)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,5 +8,14 @@ module.exports = {
     env_development: {
        NODE_ENV: "development"
     }
-  }]
-}
+  }],
+
+  deploy : {
+    production : {
+       "ref"  : "origin/v2.0.0",
+       "repo" : "git@github.com:bcat1023/cgrs-application-server.git",
+       "path" : "/home/gillen/CGRS",
+       "post-deploy" : "npm install"
+    }
+  }
+};
